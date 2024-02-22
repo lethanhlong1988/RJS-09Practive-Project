38bed559df1425ff89aa653fa35dd7855b3af22c
long le thanh <61105646+lethanhlong1988@users.noreply.github.com>
2024-02-22 08:31:45 +0000

    2024-02-22-04

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,44 @@
+import { useState } from "react";
+
 import ProjectsSidebar from "./components/ProjectsSidebar";
+import NewProject from "./components/NewProject";
+import NoProjectSelected from "./components/NoProjectSelected";
 
 function App() {
+  const [projectsState, setProjectsState] = useState({
+    selectedProjectId: undefined,
+    projects: [],
+  });
+
+  function handleStartAddProject() {
+    console.log("click!");
+    setProjectsState((prevState) => {
+      return {
+        ...prevState,
+        selectedProjectId: null,
+      };
+    });
+  }
+
+  let content;
+  switch (projectsState.selectedProjectId) {
+    case null:
+      content = <NewProject />;
+      console.log(content);
+      break;
+    case undefined:
+      content = <NoProjectSelected onStartAddProject={handleStartAddProject} />;
+      console.log(content);
+      break;
+
+    default:
+      break;
+  }
   return (
-    <main>
-      <ProjectsSidebar />
+    <main className="h-screen my-8 flex gap-8">
+      <ProjectsSidebar onStartAddProject={handleStartAddProject} />
+
+      {content}
     </main>
   );
 }
