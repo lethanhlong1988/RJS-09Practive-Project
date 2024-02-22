d8bc535e212bb21709124e43564697696411f0d9
long le thanh <61105646+lethanhlong1988@users.noreply.github.com>
2024-02-22 00:08:56 +0000

    240222-01

---
+++ src/components/NewProject.jsx
@@ -0,0 +1,15 @@
+export default function NewProject() {
+  return (
+    <div className="bg-sky-500">
+      <menu>
+        <li>
+          <button>Cancel</button>
+        </li>
+        <li>
+          <button>Save</button>
+        </li>
+      </menu>
+      <div>Input</div>
+    </div>
+  );
+}
